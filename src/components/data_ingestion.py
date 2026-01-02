77979f4ffe31dd88bd8e4042c9139da72779f73e
Pabitra-M <pm.pabitra.mondal.2003@gmail.com>
2026-01-02 21:08:27 +0530

    update3

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,10 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTranformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 
 @dataclass
@@ -56,5 +59,12 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingesation()
+    train_data, test_data = obj.initiate_data_ingesation()
 
+    data_transformation = DataTranformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+
+    modeltrainer = ModelTrainer()
+
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
