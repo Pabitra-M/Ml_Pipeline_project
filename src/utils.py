51afc21540a237f73ea5023e7df8f863a076e728
Pabitra-M <pm.pabitra.mondal.2003@gmail.com>
2026-01-03 20:00:46 +0530

    update5

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -53,6 +54,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, model, param):
      
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
